a02932e73730d1afc622c45fc4b44774c04713b5
JonLz <jon.lazar@gmail.com>
2012-10-15 19:19:47 -0400

    Cleanup

--- c2f.rb
+++
@@ -1,6 +0,0 @@
-celsius = File.read("temp.dat")
-print "Converting " + celsius + " degrees celsius to fahrenheit"
-fahrenheit = celsius.to_i * 9 / 5 + 32
-fh = File.new("temp.out", "w")
-fh.puts fahrenheit
-fh.close
